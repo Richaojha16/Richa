01cee9cbc8d6da84922c14f27fc942ae44e70460
Richaojha16 <55787581+Richaojha16@users.noreply.github.com>
2019-09-25 15:32:59 +0530

    Update map.init.js

--- assets/js/map.init.js
+++ assets/js/map.init.js
@@ -11,7 +11,7 @@ function init() {
 		disableDefaultUI: true,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(12.898360, 77.617950), // New York
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
@@ -32,7 +32,8 @@ function init() {
     }
 
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(40.6700, -73.9400),
+        //position: new google.maps.LatLng(40.6700, -73.9400),
+	     position: new google.maps.LatLng(12.898360, 77.617950),
         map: map,
         optimized: false,
         icon: image
